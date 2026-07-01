7195bc9011b60fb13bac06eb237ccceac885495e
jayy46574-creator <jayy46574@gmail.com>
2026-07-01 15:56:23 +0100

    Added CORS support

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(cors());
 
